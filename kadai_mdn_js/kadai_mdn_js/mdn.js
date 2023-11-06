b3b7b6bcdbcd7b943c673ddac1914152c483ede6
kendochourai <147883790+kendochourai@users.noreply.github.com>
2023-11-06 12:17:08 +0900

    kadai_mdn_jsを追加

---
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -0,0 +1,16 @@
+var today = new Date([2023, 8, 22]);
+
+
+
+console.log(today);
+
+
+
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+
+console.log(year + '年');
+console.log(month + '月');
+console.log(day + '日');
