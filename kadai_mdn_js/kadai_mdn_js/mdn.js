4d07704dc17ebeefb73582956336ab1331377ed6
kendochourai <147883790+kendochourai@users.noreply.github.com>
2023-11-06 15:20:31 +0900

    Add files via upload

---
+++ kadai_mdn_js/kadai_mdn_js/mdn.js
@@ -0,0 +1,14 @@
+var today = new Date([2023, 11, 6]);
+
+
+
+console.log(today);
+
+
+
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+
+console.log(year + '年' + month + '月' + day + '日');
